309901f0502037f980f659c4d642a54590bb7a0c
Fiddler25 <rager.mka.ti@icloud.com>
2022-08-30 17:57:57 +0900

    StartFromPreInterview

--- src/presentation/screening/endpoints.go
+++ src/presentation/screening/endpoints.go
@@ -6,6 +6,28 @@ import (
 	"github.com/go-kit/kit/endpoint"
 )
 
+// StartFromPreInterview
+type (
+	startFromPreInterviewRequest struct {
+		ApplicantEmailAddress string `json:"applicant_email_address"`
+	}
+
+	startFromPreInterviewResponse struct {
+		Err error `json:"error,omitempty"`
+	}
+)
+
+func (r startFromPreInterviewResponse) error() error { return r.Err }
+
+func makeStartFromPreInterview(uc screening.ScreeningUseCase) endpoint.Endpoint {
+	return func(_ context.Context, request interface{}) (interface{}, error) {
+		req := request.(startFromPreInterviewRequest)
+		err := uc.StartFromPreInterview(req.ApplicantEmailAddress)
+		return startFromPreInterviewResponse{Err: err}, nil
+	}
+}
+
+// Apply
 type (
 	applyRequest struct {
 		ApplicantEmailAddress string `json:"applicant_email_address"`
--- src/presentation/screening/transport.go
+++ src/presentation/screening/transport.go
@@ -15,6 +15,11 @@ import (
 func MakeHandler(ctx context.Context, client *ent.Client) http.Handler {
 	screeningRepo := is.NewScreeningRepository(ctx, client)
 
+	startFromPreInterviewHandler := kithttp.NewServer(
+		makeStartFromPreInterview(us.NewScreeningUseCase(screeningRepo)),
+		decodeStartFromPreInterviewRequest,
+		encode.Response,
+	)
 	applyHandler := kithttp.NewServer(
 		makeApply(us.NewScreeningUseCase(screeningRepo)),
 		decodeApplyRequest,
@@ -23,9 +28,18 @@ func MakeHandler(ctx context.Context, client *ent.Client) http.Handler {
 
 	r := mux.NewRouter()
 
+	r.Handle("/screening/v1/start_from_pre_interview", startFromPreInterviewHandler).Methods("POST")
 	r.Handle("/screening/v1/apply", applyHandler).Methods("POST")
 
 	return r
+}
+
+func decodeStartFromPreInterviewRequest(_ context.Context, r *http.Request) (interface{}, error) {
+	var req startFromPreInterviewRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		return nil, err
+	}
+	return req, nil
 }
 
 func decodeApplyRequest(_ context.Context, r *http.Request) (interface{}, error) {
