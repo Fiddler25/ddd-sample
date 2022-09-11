a274f94e9fe23f55790c509de705295001572618
Fiddler25 <rager.mka.ti@icloud.com>
2022-09-11 10:48:51 +0900

    schema修正

--- ent/schema/screening.go
+++ ent/schema/screening.go
@@ -1,13 +1,10 @@
 package schema
 
 import (
-	"ddd-sample/ent/schema/def"
-	"ddd-sample/ent/schema/mapping"
 	"ddd-sample/ent/schema/property"
 	"entgo.io/ent"
 	"entgo.io/ent/dialect"
 	"entgo.io/ent/schema/field"
-	"time"
 )
 
 type ScreeningID int
@@ -16,13 +13,18 @@ type Screening struct {
 	ent.Schema
 }
 
+func (Screening) Mixin() []ent.Mixin {
+	return []ent.Mixin{
+		TimeMixin{},
+	}
+}
+
 func (Screening) Fields() []ent.Field {
 	return []ent.Field{
 		field.String("id").MaxLen(50).NotEmpty().Unique().Immutable(),
 		field.Enum("screening_status").GoType(property.ScreeningStatus("")).Default(string(property.NotApplied)).Comment("採用選考ステータス"),
-		field.Time("apply_date").SchemaType(mapping.Date).Optional().Nillable().Comment("応募日"),
+		field.Time("apply_date").SchemaType(map[string]string{dialect.MySQL: "date"}).Optional().Nillable().Comment("応募日"),
 		field.String("applicant_email_address").SchemaType(map[string]string{dialect.MySQL: "varchar(50)"}).Comment("応募者メールアドレス"),
-		field.Time("created_at").SchemaType(mapping.DateTime).Default(time.Now).Annotations(def.Created),
 	}
 
 }
